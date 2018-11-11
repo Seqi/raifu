06040a2518caac296e945dd14982343ca19b9a68
seqi <tomseqi@live.co.uk>
2018-11-11 20:07:28 +0000

    Fixed tests

--- src/app/core/App.test.js
+++ src/app/core/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 it('renders without crashing', () => {
+	console.log('Running in ', process.env.NODE_ENV)
 	const div = document.createElement('div')
 	ReactDOM.render(<App />, div)
 	ReactDOM.unmountComponentAtNode(div)
--- src/app/core/auth/AuthPage.test.js
+++ src/app/core/auth/AuthPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AuthPage from 'AuthPage.js'
+import AuthPage from './AuthPage.js'
 
 it('renders without crashing', () => {
 	const div = document.createElement('div')
