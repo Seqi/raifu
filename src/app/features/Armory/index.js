3688d03c532fa1f20b280a3bfad65b386982ade2
seqi <tomseqi@live.co.uk>
2018-11-18 14:51:12 +0000

    Added gear list

--- src/app/features/Armory/index.js
+++ src/app/features/Armory/index.js
@@ -1,6 +1,6 @@
 import PrimariesList from './Primaries/PrimariesList'
 import SecondariesList from './Secondaries/SecondariesList'
 import AttachmentsList from './Attachments/AttachmentsList'
-import TacGearList from './TacGearList'
+import GearList from './Gear/GearList'
 
-export { PrimariesList, SecondariesList, AttachmentsList, TacGearList }
+export { PrimariesList, SecondariesList, AttachmentsList, GearList }
