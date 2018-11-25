9b57af87e0d83a3673e5c63655b1fd21afd1a8f8
seqi <tomseqi@live.co.uk>
2018-11-25 18:04:49 +0000

    used react env var names

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,10 +1,10 @@
 export default {
 	firebase: {
-		apiKey: process.env.FIREBASE_APIKEY,
-		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-		databaseURL: process.env.FIREBASE_DATABASE_URL,
-		projectId: process.env.FIREBASE_PROJECT_ID,
-		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+		apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+		authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+		databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+		projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+		storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 	}
 }
