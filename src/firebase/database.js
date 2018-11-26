0d0f6b7e962c5b82074dbeac4ea0281795b7ed44
seqi <tomseqi@live.co.uk>
2018-11-26 20:34:33 +0000

    Fixed database pathing

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -6,41 +6,43 @@ let database = client.database(config.firebase.databaseURL)
 
 function useGet(route) {
 	return new Promise((resolve, reject) => {
-		database
-			.ref(route)
+		database.ref(route)
 			.once('value', (snap) => resolve(snap.val() ? Object.values(snap.val()) : []), reject)
-	})	
+	})
 }
 
-function useCrud(route) {
+function useCrud(route, userRoute) {
 	return {
-		get: () => useGet(`${route}/${auth.user.uid}`),
+		get: () => useGet(`${route}/${auth.user.uid}/${userRoute}`),
 		getById: (id) => {
 			return new Promise((resolve, reject) => {
-				database.ref(`${route}/${auth.user.uid}/${id}`)
+				database
+					.ref(`${route}/${auth.user.uid}/${userRoute}/${id}`)
 					.once('value', (snap) => resolve(snap.val()), reject)
 			})
 		},
 		add: (props) => {
-			return database.ref(`${route}/${auth.user.uid}`)
+			return database.ref(`${route}/${auth.user.uid}/${userRoute}`)
 				.push(props)
 		}
 	}
 }
 
 export default {
-	primaries: useCrud('armory/primaries'),
-	secondaries: useCrud('armory/secondaries'),
-	attachments: useCrud('armory/attachments'),
-	gear: useCrud('armory/gear'),
+	primaries: useCrud('armory', 'primaries'),
+	secondaries: useCrud('armory', 'secondaries'),
+	attachments: useCrud('armory', 'attachments'),
+	gear: useCrud('armory', 'gear'),
 	brands: {
-		get: () => useGet('brands'),
+		get: () => useGet('brands')
 	},
 	platforms: {
-		getTypes: () => new Promise((resolve, reject) => { database.ref('platforms')
-			.once('value', (snap) => resolve(snap.val() ? Object.keys(snap.val()) : []), reject)
-		}),
+		getTypes: () =>
+			new Promise((resolve, reject) => {
+				database
+					.ref('platforms')
+					.once('value', (snap) => resolve(snap.val() ? Object.keys(snap.val()) : []), reject)
+			}),
 		get: (platform) => useGet(`platforms/${platform}`)
 	}
 }
-
