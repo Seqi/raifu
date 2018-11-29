e638911700b62ea45785d6e7a276d5ac6bd6e1ec
seqi <tomseqi@live.co.uk>
2018-11-29 17:57:52 +0000

    Cleaned up URL when navigating away from edit loadout

--- src/app/features/Loadouts/EditLoadout.js
+++ src/app/features/Loadouts/EditLoadout.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom' 
 
 class EditLoadout extends React.Component {
+	componentWillUnmount() {
+		// Clean up the url when navigating away
+		this.props.history.push('../')
+	}
+
 	render() {
 		return <div>Hello im the edit loadout page! You are editing {this.props.match.params.id}</div>
 	}
 }
 
-export default EditLoadout
+export default withRouter(EditLoadout)
