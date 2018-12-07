c5dc3acb73ed60d490a5505ece36be40c0269eca
seqi <tomseqi@live.co.uk>
2018-12-07 22:38:11 +0000

    Silenced warnings for firebase imports

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import client from '.'
 
 let auth = client.auth()
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
 import config from '../config/index'
 
 export default firebase.initializeApp(config.firebase)
