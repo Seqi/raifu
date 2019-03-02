3eb9a386dd72b17d0980fa1faa704d6b65732651
seqi <tomseqi@live.co.uk>
2019-03-02 16:20:12 +0000

    Deleted attachment lookups when deleting a weapon from armory used in a loadout with attachments on

--- src/firebase/database/entities/armory/weapons.js
+++ src/firebase/database/entities/armory/weapons.js
@@ -22,16 +22,39 @@ export default () => {
 					// Get all uses of this item in any loadouts
 					.ref(`${paths().lookups.loadouts.weapons}/${weaponId}`)
 					.once('value')
-					// Remove the weapon from the loadout
 					.then((snap) => {
-						Object.keys(snap.val() || {})
-							.forEach((loadoutId) => {
-								let path = `${paths()
-									.loadout(loadoutId).weapons}/${weaponId}`
+						let loadoutIds = Object.keys(snap.val() || {})
 
-								deletionRefs[path] = null
-							})
+						// Remove the weapon from the loadout
+						loadoutIds.forEach((loadoutId) => {
+							let path = `${paths()
+								.loadout(loadoutId).weapons}/${weaponId}`
+
+							deletionRefs[path] = null
+						})
+
+						// Check any attachments on the weapon on the loadout and remove lookup
+						return loadoutIds.map((loadoutId) => {
+							let attachmentsOnWeaponPath = paths()
+								.loadout(loadoutId)
+								.weapon(weaponId).attachments
+
+							return database
+								.ref(attachmentsOnWeaponPath)
+								.once('value')
+								.then((snap) => {
+									let attachmentIds = Object.keys(snap.val() || {})
+									attachmentIds.forEach((attachmentId) => {
+										let path = `${
+											paths().lookups.loadouts.attachments
+										}/${attachmentId}/${loadoutId}`
+										deletionRefs[path] = null
+									})
+								})
+						})
 					})
+					// Execute the attachment lookup deletions
+					.then((attachmentLookupPromises) => Promise.all(attachmentLookupPromises))
 					// Nuke!
 					.then(() => database.ref()
 						.update(deletionRefs))
