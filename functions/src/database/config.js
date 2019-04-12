a077ada0827889c2eeb22d9e1b033abf3d9c430d
seqi <tomseqi@live.co.uk>
2019-04-12 23:43:16 +0100

    Used concurrently to start functions and server from npm start

--- functions/src/database/config.js
+++ functions/src/database/config.js
@@ -2,8 +2,8 @@ const functions = require('firebase-functions')
 const config = functions.config()
 
 module.exports = {
-	user: config.db && config.db.user,
-	password: config.db && config.db.password,
-	database: config.db && config.db.name,
-	host: config.db && config.db.host
+	user: 'seqi',
+	password: 'pass@word123',
+	database: 'raifu',
+	host: '34.76.165.131'
 }
