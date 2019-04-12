95ecafccc2f7b275fb09355b7727482d6410f9ec
seqi <tomseqi@live.co.uk>
2019-04-13 00:11:20 +0100

    Replaced short-id package with shortid

--- src/app/features/Loadouts/AddLoadoutDialog.js
+++ src/app/features/Loadouts/AddLoadoutDialog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import shortid from 'short-id'
+import shortid from 'shortid'
 
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
