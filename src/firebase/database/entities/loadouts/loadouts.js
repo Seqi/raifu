1558f6ba5a7c20d51009ddcb82666f352f48900e
seqi <tomseqi@live.co.uk>
2019-04-18 22:39:35 +0100

    Added functions to call loadout gear cloud functions

--- src/firebase/database/entities/loadouts/loadouts.js
+++ src/firebase/database/entities/loadouts/loadouts.js
@@ -1,11 +1,13 @@
 import base from '../base-entity'
 import loadoutWeapons from './loadout-weapons'
+import loadoutGear from './loadout-weapons'
 
 export default () => {
 	return {
 		...base('loadouts'),
 		loadout: (loadoutId) => ({
-			weapons: loadoutWeapons(loadoutId)
+			weapons: loadoutWeapons(loadoutId),
+			gear: loadoutGear(loadoutId)
 		})
 	}
 }
