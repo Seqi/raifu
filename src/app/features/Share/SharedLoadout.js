463372a0000def49c52bfd28b81eac99f6f7e130
seqi <tomseqi@live.co.uk>
2019-06-04 22:46:12 +0100

    Moved from functional to class component due to set state chaining

--- src/app/features/Share/SharedLoadout.js
+++ src/app/features/Share/SharedLoadout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
 import SharedNotFound from './SharedNotFound'
@@ -6,52 +6,62 @@ import { Loading, Error } from 'app/shared/components'
 import LoadoutView from 'app/shared/components/Views/Loadout/LoadoutView'
 import database from '../../../firebase/database'
 
-export default function SharedLoadout(props) {
-	let [ loadout, setLoadout ] = useState({data: null, error: null, loading: true })
-	let unmounted = false
+export default class SharedLoadout extends React.Component {
 
-	useEffect(() => {
-		loadLoadout()
+	constructor(props) {
+		super(props)
 
-		return () => unmounted = true
-	}, [])
-
-	let loadLoadout = () => {
-		setLoadout({ error: null, loading: true })
-
-		database.loadouts.getById(props.match.params.loadoutId)
-			.then((loadout) => {
-				!unmounted && setLoadout({ data: loadout, loading: false, error: null })
-			})
-			.catch((err) => {
-				let error = null
-				if (err !== 'Loadout not found') {
-					error = err.message || err
-				}
-
-				!unmounted && setLoadout({ data: null, loading: false, error: error })
-			})
+		this.state = {
+			loadout: null,
+			error: null,
+			loading: true
+		}
 	}
 
-	if (loadout.loading) {
-		return <Loading />
+	componentDidMount = () => this.loadLoadout()
+
+	componentWillUnmount = () => this.unmounted = true
+
+	loadLoadout = () => {
+		this.setState({ error: null, loading: true }, () => {
+			database.loadouts.getById(this.props.match.params.loadoutId)
+				.then((loadout) => {
+					!this.unmounted && this.setState({ loadout: loadout, loading: false, error: null })
+				})
+				.catch((err) => {
+					let error = null
+					if (err !== 'Loadout not found') {
+						error = err.message || err
+					}
+
+					!this.unmounted && this.setState({ loadout: null, loading: false, error: error })
+				})
+		})	
 	}
 
-	if (loadout.error) {
-		return <Error error={ loadout.error } onRetry={ () => loadLoadout() } />
+	render() {
+		let { loadout: data, error, loading } = this.state 
+
+		if (loading) {
+			return <Loading />
+		}
+	
+		if (error) {
+			return <Error error={ error } onRetry={ () => this.loadLoadout() } />
+		}
+	
+		if (!data) {
+			return <SharedNotFound entityName={ 'Loadout' } />
+		}
+	
+		return (
+			<React.Fragment>
+				<Typography variant='h3' >
+					{ data.name }					
+				</Typography>
+	
+				<LoadoutView loadout={ data } />
+			</React.Fragment>
+		)
 	}
-
-	if (!loadout.data) {
-		return <SharedNotFound entityName={ 'Loadout' } />
-	}
-
-	return (
-		<React.Fragment>
-			<Typography variant='h3' >
-				{ loadout.data.name }					
-			</Typography>
-
-			<LoadoutView loadout={ loadout.data } />
-		</React.Fragment>
-	)
 }
