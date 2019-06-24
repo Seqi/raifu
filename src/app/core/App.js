1301e4552ac71c06378890965464c8e48338ce4a
seqi <tomseqi@live.co.uk>
2019-06-25 00:03:49 +0100

    Moved navbar outside of individual components

--- src/app/core/App.js
+++ src/app/core/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import NavBar from './layout/Navbar/Navbar'
 import AuthPage from './auth/AuthPage'
 import Main from './layout/MainApp/Main'
 import Shared from './layout/Shared/Shared'
+import HomePage from './layout/Home'
 
 class App extends Component {
 	render() {
@@ -14,9 +16,14 @@ class App extends Component {
 					<React.Fragment>
 						<Switch>
 							<Route path='/login' component={ AuthPage } />
-							<Route path='/app' component={ Main } />
-							<Route path='/share' component={ Shared } />
-							<Redirect from='/' to='/app' />
+							<Route path='/' component={ () => 
+								<React.Fragment>
+									<NavBar />
+									<Route path='/app' component={ Main } />
+									<Route path='/share' component={ Shared } />
+									<Route path='/' component={ HomePage } />
+								</React.Fragment>
+							} />
 						</Switch>
 					</React.Fragment>
 				</Router>
---
+++ src/app/core/layout/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Ump45 from 'assets/ump45.png'
+
+export default function HomePage() {
+	return (
+		<React.Fragment>
+			<div>Im a homepage</div>
+		</React.Fragment>
+	)
+}
