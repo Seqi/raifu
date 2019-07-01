ce665c0b9e85f92fff29f429ba5798b6f61efa29
seqi <tomseqi@live.co.uk>
2019-07-01 20:40:12 +0100

    Made authpage into a function

--- src/app/core/auth/AuthPage.js
+++ src/app/core/auth/AuthPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import LoginPage from './Login/LoginPage'
@@ -8,38 +8,33 @@ import authClient from '../../../firebase/auth'
 
 import './AuthPage.css'
 
-class AuthPage extends Component {
-	constructor(props) {
-		super(props)
+function AuthPage({ history }) {
+	useEffect(() => {
 		// Listen out for successful login to redirect back
 		// also handles if they're already authenticated
-		this.authUnsubscribe = authClient.onAuthChanged((user) => {
+		let authUnsubscribe = authClient.onAuthChanged((user) => {
 			if (user) {
-				this.props.history.push('/app')
+				history.push('/app')
 			}
 		})
-	}
 
-	componentWillUnmount() {
-		this.authUnsubscribe()
-	}
+		return authUnsubscribe
+	}, [])
 
-	render() {
-		return (
-			<div className='auth-container'>
-				<div className='auth-logo-container'>
-					<Logo height='250px' subtitle='Airsoft loadout management' />
-				</div>
-
-				<Router basename='/login'>
-					<Switch>
-						<Route path='/signup' component={ SignupPage } />
-						<Route path='/' component={ LoginPage } />
-					</Switch>
-				</Router>
+	return (
+		<div className='auth-container'>
+			<div className='auth-logo-container'>
+				<Logo height='250px' subtitle='Airsoft loadout management' />
 			</div>
-		)
-	}
+
+			<Router basename='/login'>
+				<Switch>
+					<Route path='/signup' component={ SignupPage } />
+					<Route path='/' component={ LoginPage } />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
 export default AuthPage
