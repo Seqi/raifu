86d480e4b0aaac2ac70a79e1529bbe94a9843b21
seqi <tomseqi@live.co.uk>
2019-12-06 23:53:08 +0000

    Converted main to function to try and fix auth loop

--- src/app/core/layout/MainApp/Main.js
+++ src/app/core/layout/MainApp/Main.js
@@ -1,7 +1,7 @@
 import './Main.css'
 
-import React, { Component } from 'react'
-import { withRouter, BrowserRouter as Router, Link, Switch, Redirect } from 'react-router-dom'
+import React, { useState, useCallback } from 'react'
+import { BrowserRouter as Router, Link, Switch, Redirect } from 'react-router-dom'
 
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
@@ -11,68 +11,52 @@ import Loadouts from 'app/features/Loadouts/Loadouts'
 import Events from 'app/features/Events/Events'
 import AuthenticatedRoute from 'app/shared/components/Auth/AuthenticatedRoute'
 
-class Main extends Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			tabIndex: this.getTabIndex()
-		}
-	}
-
-	// Fixes active tab on direct navigations
-	getTabIndex() {
+let  Main = ({ history, location }) => {
+	let [tabIndex, setTabIndex] = useState(() => {
+		// Fixes active tab on direct navigations
 		let idxMap = [
 			{ path: '/armory', idx: 0 },
 			{ path: '/loadouts', idx: 1 },
 			{ path: '/events', idx: 2 },
 		]
-
-		let currPath = this.props.location.pathname
-
+	
+		let currPath = location.pathname
+	
 		for (let map of idxMap) {
 			if (currPath.indexOf(map.path) > -1) {
 				return map.idx
 			}
 		}
-
+	
 		return 0
-	}
+	})
 
-	tabChange(evt, tabIndex) {
-		this.setState({ tabIndex })
-	}
+	let onAuthFailure = useCallback(() => {
+		history.push('/login')
+	}, [history])
 
-	onAuthFailure() {
-		this.props.history.push('/login')
-	}
+	return (
+		<React.Fragment>
+			<Router basename='/app'>
+				<React.Fragment>
+					<Tabs centered={ true } value={ tabIndex } onChange={ (evt, idx) => setTabIndex(idx) }>
+						<Tab label='Armory' component={ Link } to='/armory' />
+						<Tab label='Loadouts' component={ Link } to='/loadouts' />
+						<Tab label='Events' component={ Link } to='/events' />
+					</Tabs>
 
-	render() {
-		let { tabIndex } = this.state
-
-		return (
-			<React.Fragment>
-				<Router basename='/app'>
-					<React.Fragment>
-						<Tabs centered={ true } value={ tabIndex } onChange={ (evt, idx) => this.tabChange(evt, idx) }>
-							<Tab label='Armory' component={ Link } to='/armory' />
-							<Tab label='Loadouts' component={ Link } to='/loadouts' />
-							<Tab label='Events' component={ Link } to='/events' />
-						</Tabs>
-
-						<div className='app-window'>
-							<Switch>
-								<AuthenticatedRoute path='/armory' component={ Armory } onFail={ () => this.onAuthFailure() } />
-								<AuthenticatedRoute path='/loadouts' component={ Loadouts } onFail={ () => this.onAuthFailure() }  />
-								<AuthenticatedRoute path='/events' component={ Events } onFail={ () => this.onAuthFailure() }  />
-								<Redirect from='/' to='/armory' />
-							</Switch>
-						</div>
-					</React.Fragment>
-				</Router>
-			</React.Fragment>
-		)
-	}
+					<div className='app-window'>
+						<Switch>
+							<AuthenticatedRoute path='/armory' component={ Armory } onFail={ onAuthFailure } />
+							<AuthenticatedRoute path='/loadouts' component={ Loadouts } onFail={ onAuthFailure }  />
+							<AuthenticatedRoute path='/events' component={ Events } onFail={ onAuthFailure }  />
+							<Redirect from='/' to='/armory' />
+						</Switch>
+					</div>
+				</React.Fragment>
+			</Router>
+		</React.Fragment>
+	)
 }
 
-export default withRouter(Main)
+export default Main
