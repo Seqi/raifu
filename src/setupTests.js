0c654a8136c3487a10776c37fd0d07ab2108738a
seqi <tomseqi@live.co.uk>
2019-12-08 17:45:19 +0000

    Removed out of date test files cos not testing is hardcore

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
-
-// Surpress Material UI warning
-window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true
