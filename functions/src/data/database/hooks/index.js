9fbac64c6e12bb27acd15adb389c3ab1aee49b59
seqi <tomseqi@live.co.uk>
2020-01-11 23:52:56 +0000

    Cleanups of unused code

--- functions/src/data/database/hooks/index.js
+++ functions/src/data/database/hooks/index.js
@@ -1,5 +1,4 @@
 module.exports = {
 	applyHook: require('./apply-hook'),
-	jsonifyDates: require('./jsonify-dates'),
 	orderLoadoutItems: require('./order-loadout-items'),
 }
--- functions/src/data/database/hooks/order-loadout-items.js
+++ functions/src/data/database/hooks/order-loadout-items.js
@@ -7,29 +7,24 @@ module.exports = function orderLoadoutItems(loadout) {
 		return 
 	}
 
-	console.log(loadout, '#######################################', loadout.dataValues)
-
 	loadout.dataValues.weapons.sort(dateSortByKey('loadout_weapon'))
 
 	if (loadout.dataValues.gear) {
 		loadout.dataValues.gear.sort(dateSortByKey('loadout_gear'))	
-		// loadout.gear.forEach(gear => delete gear.loadout_gear)
+		loadout.gear.forEach(gear => delete gear.loadout_gear)
 	}
 
 	if (loadout.dataValues.clothing) {
 		loadout.dataValues.clothing.sort(dateSortByKey('loadout_clothing'))	
-		// loadout.gear.forEach(gear => delete gear.loadout_gear)
+		loadout.gear.forEach(gear => delete gear.loadout_clothing)
 	}
 	
 	loadout.dataValues.weapons.forEach(weapon => {
-		// delete weapon.loadout_weapon
+		delete weapon.loadout_weapon
 
 		if (!weapon.dataValues.attachments) {
-			return
+			weapon.dataValues.attachments.sort(dateSortByKey('loadout_weapon_attachment'))
+			weapon.dataValues.attachments.forEach(attachment => delete attachment.loadout_weapon_attachment)
 		}
-
-		weapon.dataValues.attachments.sort(dateSortByKey('loadout_weapon_attachment'))
-
-		// weapon.attachments.forEach(attachment => delete attachment.loadout_weapon_attachment)
 	})
 }
