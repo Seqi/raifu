114e070194773c1f4a8425a2dd09f1a1a7400c3a
seqi <tomseqi@live.co.uk>
2020-01-12 22:00:33 +0000

    Changed database init to plain old module

--- functions/src/data/database/database.js
+++ functions/src/data/database/database.js
@@ -3,23 +3,18 @@ const shortid = require('shortid')
 
 const config = require('./config')
 
-let sequelize
-
-module.exports = () => {
-	if (!sequelize) {
-		sequelize = new Sequelize(config.database, config.user, config.password, {
-			host: config.host,
-			dialect: 'postgres',
-			dialectOptions: {
-				useUTC: false
-			}
-		})
-
-		// Adding hooks with this method adds permanent hooks, ensuring that
-		// these hooks are still run, even when other hooks are defined at
-		// the entitiy level
-		sequelize.addHook('beforeCreate', model => model.id = shortid.generate())
+console.log('############################# init sequelize #######################')
+let sequelize = new Sequelize(config.database, config.user, config.password, {
+	host: config.host,
+	dialect: 'postgres',
+	dialectOptions: {
+		useUTC: false
 	}
+})
 
-	return sequelize
-}
+// Adding hooks with this method adds permanent hooks, ensuring that
+// these hooks are still run, even when other hooks are defined at
+// the entitiy level
+sequelize.addHook('beforeCreate', model => model.id = shortid.generate())
+
+module.exports = sequelize
