ab51e73601ef770e382068aeb7c40c6d3d5d4a44
Seqi <tomseqi@live.co.uk>
2020-01-26 01:14:01 +0000

    Moved delete button into self contained wrapper component (#182)
    * Moved loadout button into self contained wrapper component
    
    * Clean ups
    
    * Fixed delete loadout weapon attachment not passing in weapon id

--- src/app/shared/cards/entities/LoadoutCard.js
+++ src/app/shared/cards/entities/LoadoutCard.js
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types'
 
 import { Card, CardHeader, CardContent } from '@material-ui/core'
 
+import Deletable from 'app/shared/actions/Deletable'
 import WeaponDisplay from 'app/shared/images/WeaponDisplay'
-import DeleteButton from 'app/shared/buttons/DeleteButton'
 
 import './LoadoutCard.css'
 
 export default function LoadoutCard({loadout, canDelete, onClick, onDelete, style }) {	
 	return (
 		<Card style={ style } onClick={ onClick } className='card loadout-card' >
-			{ canDelete && <DeleteButton onClick={ onDelete } /> }
-
-			<CardHeader title={ loadout.getTitle() } subheader={ loadout.getSubtitle() } className='card-header' />
+			<Deletable canDelete={ canDelete } onDelete={ onDelete } dialogTitle={ loadout.getTitle() }>
+				<CardHeader title={ loadout.getTitle() } subheader={ loadout.getSubtitle() } className='card-header' />
 			
-			<CardContent className='card-content'>
-				<WeaponDisplay weapons={ loadout.weapons } />
-			</CardContent>
+				<CardContent className='card-content'>
+					<WeaponDisplay weapons={ loadout.weapons } />
+				</CardContent>
+			</Deletable>
 		</Card>
 	)
 }
@@ -26,24 +26,7 @@ LoadoutCard.propTypes = {
 	loadout: PropTypes.shape({
 		getTitle: PropTypes.func.isRequired,
 		getSubtitle: PropTypes.func.isRequired,
-		weapons: PropTypes.arrayOf(PropTypes.shape({			
-			platform: PropTypes.string.isRequired,
-			model: PropTypes.string,
-			brand: PropTypes.string,
-			nickname: PropTypes.string,
-			type: PropTypes.string.isRequired,
-			getTitle: PropTypes.func.isRequired,
-			getSubtitle: PropTypes.func.isRequired,
-			attachments: PropTypes.arrayOf(PropTypes.shape({			
-				platform: PropTypes.string.isRequired,
-				model: PropTypes.string,
-				brand: PropTypes.string,
-				nickname: PropTypes.string,
-				type: PropTypes.string.isRequired,
-				getTitle: PropTypes.func.isRequired,
-				getSubtitle: PropTypes.func.isRequired,
-			}))
-		})),
+		weapons: PropTypes.array
 	}).isRequired,	
 	canDelete: PropTypes.bool,
 	onClick: PropTypes.func,
