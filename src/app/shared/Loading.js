1bbdbede1e01c89e2ec4411110f91a83a9d4506a
seqi <tomseqi@live.co.uk>
2020-02-16 23:47:00 +0000

    Display a message if loading takes too long

--- src/app/shared/Loading.js
+++ src/app/shared/Loading.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Loading.css'
 
+const REASSURANCE_TIME = 3000
+const REASSURANCE_MESSAGE = 'Hold tight! This won\'t take a second.'
+
 export default function Loading() {
-	return <div className='lds-dual-ring' />
+	let [showReassurance, setShowReassurance] = useState(false)
+
+	useEffect(() => {
+		let timeout = setTimeout(() => setShowReassurance(true), REASSURANCE_TIME)
+
+		return () => clearTimeout(timeout)			
+	}, [])
+
+	return (
+		<div>
+			<div className='lds-dual-ring' />
+
+			{ showReassurance && (
+				<div className='reassurance-text'>
+					{ REASSURANCE_MESSAGE }
+				</div>
+			)}
+		</div>
+	)
 }
