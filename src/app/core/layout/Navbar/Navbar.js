a3abb126acd161dd03d71b7c8d448b1eb6a85143
seqi <tomseqi@live.co.uk>
2020-02-20 00:24:23 +0000

    Moved navbar to use material ui styles

--- src/app/core/layout/Navbar/Navbar.js
+++ src/app/core/layout/Navbar/Navbar.js
@@ -1,19 +1,31 @@
-import './Navbar.css'
 import React, { useContext } from 'react'
+
+import { Box, makeStyles } from '@material-ui/core'
 
 import { UserContext } from 'app/core/auth/contexts'
 import AuthenticatedUserMenu from './AuthenticatedUserMenu'
 import UnauthenticatedUserMenu from './UnauthenticatedUserMenu'
 
+let useStyles = makeStyles(theme => ({
+	root: {
+		padding: theme.spacing(5, 7, 0),
+		
+		[theme.breakpoints.down('sm')]: {			
+			padding: theme.spacing(2, 1, 0),
+		}
+	}
+}))
+
 let Navbar = () => {
 	let user = useContext(UserContext)
+	let classes = useStyles()
 
 	return (
-		<div className='navbar'>
+		<Box display='flex' alignItems='center' className={ classes.root }> 
 			{ user ? 
 				<AuthenticatedUserMenu user={ user } /> : 
 				<UnauthenticatedUserMenu /> }
-		</div>
+		</Box>
 	)
 }
 
