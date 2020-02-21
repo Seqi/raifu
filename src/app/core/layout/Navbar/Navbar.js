5259d67ff198e3511230fd3fe915016ca538d043
seqi <tomseqi@live.co.uk>
2020-02-21 23:30:07 +0000

    Moved NavBar to styled components

--- src/app/core/layout/Navbar/Navbar.js
+++ src/app/core/layout/Navbar/Navbar.js
@@ -1,31 +1,28 @@
 import React, { useContext } from 'react'
 
-import { Box, makeStyles } from '@material-ui/core'
+import { Box, styled } from '@material-ui/core'
 
 import { UserContext } from 'app/core/auth/contexts'
 import AuthenticatedUserMenu from './AuthenticatedUserMenu'
 import UnauthenticatedUserMenu from './UnauthenticatedUserMenu'
 
-let useStyles = makeStyles(theme => ({
-	root: {
-		padding: theme.spacing(5, 7, 0),
+let NavbarContainer = styled(Box)(({ theme }) => ({
+	padding: theme.spacing(5, 7, 0),
 		
-		[theme.breakpoints.down('sm')]: {			
-			padding: theme.spacing(2, 1, 0),
-		}
+	[theme.breakpoints.down('sm')]: {			
+		padding: theme.spacing(2, 1, 0),
 	}
 }))
 
 let Navbar = () => {
 	let user = useContext(UserContext)
-	let classes = useStyles()
 
 	return (
-		<Box display='flex' alignItems='center' className={ classes.root }> 
+		<NavbarContainer display='flex' alignItems='center'> 
 			{ user ? 
 				<AuthenticatedUserMenu user={ user } /> : 
 				<UnauthenticatedUserMenu /> }
-		</Box>
+		</NavbarContainer>
 	)
 }
 
