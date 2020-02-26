dd64108d6a4babdaad4df6e023de4224595cc4c0
seqi <tomseqi@live.co.uk>
2020-02-26 17:32:34 +0000

    Moed add button to material ui styles

--- src/app/shared/buttons/AddButton.js
+++ src/app/shared/buttons/AddButton.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Box, withTheme, IconButton } from '@material-ui/core'
+import { Box, IconButton, styled } from '@material-ui/core'
 
-const buttonStyle = {
+const AddIconButton = styled(IconButton)(({ theme }) => ({
 	position: 'absolute',
 	top: '50%',
 	left: '50%',
 	transform: 'translateX(-50%) translateY(-50%)',
 
-	backgroundColor: 'inherit',
-	cursor: 'pointer',
-	border: '0',
-	borderRadius: '50%',
-
 	fontSize: '4rem',
-}
 
-function AddButton({ theme, onClick }) {
+	'& i': {
+		color: theme.palette.primary.main
+	},
+	// Disable ripple properly
+	'&:hover': {
+		backgroundColor: 'initial'
+	}
+}))
+
+function AddButton({ onClick }) {
 	return (
 		<Box display='flex' flex='1' height='100%' position='relative'>
-			<IconButton onClick={ onClick } type='button' style={ buttonStyle }>
-				<i style={ { color: theme.palette.primary.main } } className='fa fa-plus' />
-			</IconButton>
+			<AddIconButton onClick={ onClick }>
+				<i className='fa fa-plus' />
+			</AddIconButton>
 		</Box>
 	)
 }
@@ -35,4 +38,4 @@ AddButton.defaultProps = {
 	onClick: () => {}
 }
 
-export default withTheme(AddButton)
+export default AddButton
