2f9c2ff0bd4d83140861f01cdc8f7d708230ecd2
seqi <tomseqi@live.co.uk>
2020-02-26 00:52:22 +0000

    Moved loading to material styled components

--- src/app/shared/Loading.js
+++ src/app/shared/Loading.js
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from 'react'
 
-import { useTheme } from '@material-ui/core'
+import { Box, Fade, Typography, styled } from '@material-ui/core'
 
+// Keyframes doesn't seem to work with styled components?
+// falling back to css file for now...
 import './Loading.css'
 
-const REASSURANCE_TIME = 3000
+const REASSURANCE_TIME = 50
 const REASSURANCE_MESSAGE = 'Hold tight! This won\'t take a second.'
+
+const LoadingContainer = styled(Box)(({ theme }) => ({
+	'& i': {
+		color: theme.palette.background.paper,
+		fontSize: '10rem',
+
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '7rem',
+		},
+	}
+}))
 
 export default function Loading() {
 	let [showReassurance, setShowReassurance] = useState(false)
-	let theme = useTheme()
 
 	useEffect(() => {
 		let timeout = setTimeout(() => setShowReassurance(true), REASSURANCE_TIME)
-
 		return () => clearTimeout(timeout)			
 	}, [])
 
 	return (
-		<div style={ {textAlign: 'center'} }>
-			<i 
-				style={ { color: theme.palette.background.paper } } 
-				className='fa fa-crosshairs load-icon'
-			/>
+		<LoadingContainer textAlign='center'>
+			<i className='fa fa-crosshairs load-icon'/>
 
-			{ showReassurance && (
-				<div className='fade-in'>
-					{ REASSURANCE_MESSAGE }
-				</div>
-			)}
-		</div>
+			<Fade in={ showReassurance } timeout={ 1500 }>
+				<Typography variant='h6'>{ REASSURANCE_MESSAGE }</Typography>
+			</Fade>
+		</LoadingContainer>
 	)
 }
