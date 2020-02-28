d805fd902e079bfb0dc578e32e559767a092612e
seqi <tomseqi@live.co.uk>
2020-02-28 02:43:47 +0000

    Gave the app more padding for wider screens

--- src/app/core/pages/App/App.js
+++ src/app/core/pages/App/App.js
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense, useState, useCallback } from 'react'
 import { BrowserRouter as Router, Link, Switch, Redirect } from 'react-router-dom'
-
-import { Box, Container, Tabs, Tab, useMediaQuery, useTheme } from '@material-ui/core'
+import { Box, Container, Tabs, Tab, useMediaQuery, styled } from '@material-ui/core'
 
 import LoadingOverlay from 'app/shared/state/loading/LoadingOverlay'
 import AuthenticatedRoute from '../../auth/AuthenticatedRoute'
@@ -9,6 +8,12 @@ import AuthenticatedRoute from '../../auth/AuthenticatedRoute'
 const Armory = lazy(() => import('app/features/armory'))
 const LoadoutRouter = lazy(() => import('app/features/loadouts/LoadoutRouter'))
 const EventRouter = lazy(() => import('app/features/events/EventRouter'))
+
+let PaddedContainer = styled(Container)(({ theme }) => ({
+	[theme.breakpoints.up('lg')]: {
+		padding: theme.spacing(0, 5)
+	}
+}))
 
 let App = ({ location, history }) => {
 	let [tabIndex, setTabIndex] = useState(() => {
@@ -32,21 +37,20 @@ let App = ({ location, history }) => {
 
 	let onAuthFailure = useCallback(() => history.push('/login'), [history])
 
-	let theme = useTheme()
-	let isMobile = useMediaQuery(theme.breakpoints.down('xs'))
+	let isMobile = useMediaQuery(theme => theme.breakpoints.down('xs'))
 
 	return (
 		<Router basename='/app'>
 			<React.Fragment>				
-				<Container disableGutters={ isMobile } maxWidth={ false }>
+				<PaddedContainer disableGutters={ isMobile } maxWidth={ false }>
 					<Tabs width='100%' variant='fullWidth' centered={ true } value={ tabIndex } onChange={ (evt, idx) => setTabIndex(idx) }>
 						<Tab label='Armory' component={ Link } to='/armory' />
 						<Tab label='Loadouts' component={ Link } to='/loadouts' />
 						<Tab label='Events' component={ Link } to='/events' />
 					</Tabs>
-				</Container>
+				</PaddedContainer>
 
-				<Container maxWidth={ false }>
+				<PaddedContainer maxWidth={ false }>
 					<Box paddingY={ 2 }>
 						<Suspense fallback={ <LoadingOverlay /> }>
 							<Switch>
@@ -57,7 +61,7 @@ let App = ({ location, history }) => {
 							</Switch>
 						</Suspense>
 					</Box>
-				</Container>
+				</PaddedContainer>
 			</React.Fragment>
 		</Router>
 	)
