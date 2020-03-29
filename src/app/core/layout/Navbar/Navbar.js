dbbe503ba6208dcfb33f67c8ed96ec50e03cc913
seqi <tomseqi@live.co.uk>
2020-03-29 03:22:35 +0100

    Removed unused import

--- src/app/core/layout/Navbar/Navbar.js
+++ src/app/core/layout/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useCallback } from 'react'
+import React, { useContext, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 
 import { Box, styled, Button, IconButton, Badge, Tooltip } from '@material-ui/core'
