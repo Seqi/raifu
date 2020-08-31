8d473bdad1f9155d61e85d7b06d1b795b59083c0
seqi <tomseqi@live.co.uk>
2020-09-01 00:52:32 +0100

    Make the time relative on the event header

--- src/app/features/events/Event/EventHeader.js
+++ src/app/features/events/Event/EventHeader.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import * as moment from 'moment'
 
 import { Box, Tooltip, Chip } from '@material-ui/core'
 
 import ReactiveTitle from 'app/shared/text/ReactiveTitle'
 
 function EventHeader({ event }) {
+	const eventDate = moment(event.date)
+
 	return (
 		<React.Fragment>
 			<ReactiveTitle>
 				{event.getTitle()}
 
-				<Box component='span' paddingLeft={1}>
+				<Box component='span' paddingLeft={ 1 }>
 					{event.public ? (
 						<Tooltip placement='right' title='Event can be joined by others!'>
 							<Chip label='Public' size='small' color='primary' />
@@ -25,7 +28,10 @@ function EventHeader({ event }) {
 			</ReactiveTitle>
 
 			<ReactiveTitle variant='h4' mobileVariant='h5'>
-				{event.location} @ {event.date.toLocaleString()}
+				{event.location}
+				<Tooltip placement='bottom' title={ event.date.toLocaleString() }>
+					<span> {eventDate.fromNow()}</span>
+				</Tooltip>
 			</ReactiveTitle>
 		</React.Fragment>
 	)
@@ -39,6 +45,6 @@ EventHeader.propTypes = {
 		public: PropTypes.bool.isRequired,
 		location: PropTypes.string.isRequired,
 		date: PropTypes.object.isRequired,
-		getTitle: PropTypes.func.isRequired,
-	}).isRequired,
+		getTitle: PropTypes.func.isRequired
+	}).isRequired
 }
