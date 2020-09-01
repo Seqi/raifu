ee6be1f5d0e46268a1e49469d9a1ca7f64799a20
seqi <tomseqi@live.co.uk>
2020-09-01 02:01:25 +0100

    Added animations

--- src/app/features/events/EventList/WeeklyView/EventWeeklyView.js
+++ src/app/features/events/EventList/WeeklyView/EventWeeklyView.js
@@ -2,7 +2,9 @@ import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import * as moment from 'moment'
 
-import { styled, Box } from '@material-ui/core'
+import { styled, Box, Fade } from '@material-ui/core'
+
+import StaggeredAnimation from 'app/shared/animations/StaggeredAnimation'
 
 import EventWeekSelect from './EventWeekSelect'
 import EventDay from './EventDay'
@@ -16,25 +18,34 @@ const EventWeeklyViewContainer = styled(Box)({
 const EventWeeklyView = ({ events, onEventSelected, onSlotSelected }) => {
 	let [week, setWeek] = useState([])
 
-	const getEventsForDay = useCallback((day) => events.filter((event) => moment(event.date)
-		.isSame(day, 'day')), [
-		events
-	])
+	const getEventsForDay = useCallback(
+		(day) => {
+			return events.filter((event) => moment(event.date)
+				.isSame(day, 'day'))
+		},
+		[events]
+	)
 
 	return (
 		<EventWeeklyViewContainer>
 			<EventWeekSelect onWeekChange={ (newWeek) => setWeek(newWeek) } />
 
 			<Box display='flex' flexDirection='column' flex={ 1 } paddingTop={ 2 }>
-				{week.map((day) => (
-					<EventDay
-						key={ +day }
-						events={ getEventsForDay(day) }
-						day={ day }
-						onEventSelected={ onEventSelected }
-						onSlotSelected={ onSlotSelected }
-					/>
-				))}
+				<StaggeredAnimation minInterval={ 0.1 } freezeAfterInitial={ false }>
+					{week.map((day) => (
+						<Fade key={ +day } timeout={ 1000 } in={ true }>
+							{/* Necessary for animation to work */}
+							<Box display='flex' flex={ 1 }>
+								<EventDay
+									events={ getEventsForDay(day) }
+									day={ day }
+									onEventSelected={ onEventSelected }
+									onSlotSelected={ onSlotSelected }
+								/>
+							</Box>
+						</Fade>
+					))}
+				</StaggeredAnimation>
 			</Box>
 		</EventWeeklyViewContainer>
 	)
