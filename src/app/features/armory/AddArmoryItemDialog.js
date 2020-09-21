ef69a5e55a8265897ddb0d3ed25a2b3f7097f95a
seqi <tomseqi@live.co.uk>
2020-09-22 00:59:12 +0100

    Few tweaks to make the resource select a bit cleaner and more reacty

--- src/app/features/armory/AddArmoryItemDialog.js
+++ src/app/features/armory/AddArmoryItemDialog.js
@@ -8,9 +8,9 @@ import { Autocomplete } from '@material-ui/lab'
 import { Error } from 'app/shared/state'
 import ResourceSelect from 'app/shared/resources/ResourceSelect'
 
-import { brands, platforms } from 'app/data/constants'
+import { brands } from 'app/data/constants'
 
-let AddArmoryItemDialog = ({ resourceTitle, resourceKey, resourceName, isOpen, onSave, onClose }) => {
+const AddArmoryItemDialog = ({ resourceTitle, resourceKey, resourceName, isOpen, onSave, onClose }) => {
 	let [error, setError] = useState(null)
 	let { register, unregister, setValue, handleSubmit, formState } = useForm({ mode: 'onChange' })
 
@@ -38,45 +38,43 @@ let AddArmoryItemDialog = ({ resourceTitle, resourceKey, resourceName, isOpen, o
 	)
 
 	return (
-		<Dialog fullWidth={true} open={isOpen} onClose={onClose}>
-			<form onSubmit={handleSubmit(handleSave)}>
+		<Dialog fullWidth={ true } open={ isOpen } onClose={ onClose }>
+			<form onSubmit={ handleSubmit(handleSave) }>
 				<DialogTitle>Add {resourceTitle}</DialogTitle>
 
 				<DialogContent>
-					{error && <Error error={error} fillBackground={true} />}
+					{error && <Error error={ error } fillBackground={ true } />}
 
 					<ResourceSelect
-						resourceOptions={platforms[resourceKey]}
-						getOptionLabel={(option) => option.resource}
-						groupBy={(option) => option.type}
-						onChange={(value) => setValue([{ type: value.type }, { platform: value.platform }], true)}
-						renderInput={(params) => <TextField {...params} fullWidth={true} label={resourceName} />}
+						inputLabel={ resourceName }
+						resourceType={ resourceKey }
+						onChange={ (value) => setValue([{ type: value.type }, { platform: value.platform }], true) }
 					/>
 
 					<Autocomplete
-						options={brands}
-						freeSolo={true}
-						renderInput={(params) => (
-							<TextField name='brand' inputRef={register} {...params} fullWidth={true} label='Brand' />
-						)}
+						options={ brands }
+						freeSolo={ true }
+						renderInput={ (params) => (
+							<TextField name='brand' inputRef={ register } { ...params } fullWidth={ true } label='Brand' />
+						) }
 					/>
 
 					<TextField
 						name='model'
 						label='Model'
 						type='text'
-						fullWidth={true}
-						inputRef={register}
+						fullWidth={ true }
+						inputRef={ register }
 						helperText='E.g. Raider 2.0, Trident MK-II, Nighthawk'
 					/>
 
-					<TextField name='nickname' label='Nickname' type='text' fullWidth={true} inputRef={register} />
+					<TextField name='nickname' label='Nickname' type='text' fullWidth={ true } inputRef={ register } />
 				</DialogContent>
 
 				<DialogActions>
-					<Button onClick={onClose}>Cancel</Button>
+					<Button onClick={ onClose }>Cancel</Button>
 					<Button
-						disabled={formState.isSubmitting || !formState.isValid}
+						disabled={ formState.isSubmitting || !formState.isValid }
 						type='submit'
 						variant='contained'
 						color='primary'
@@ -95,7 +93,7 @@ AddArmoryItemDialog.propTypes = {
 	resourceName: PropTypes.string.isRequired,
 	isOpen: PropTypes.bool.isRequired,
 	onClose: PropTypes.func.isRequired,
-	onSave: PropTypes.func.isRequired,
+	onSave: PropTypes.func.isRequired
 }
 
 export default AddArmoryItemDialog
--- src/app/shared/resources/ResourceSelect.js
+++ src/app/shared/resources/ResourceSelect.js
@@ -1,42 +1,56 @@
-import React, { useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import { styled, Popper } from '@material-ui/core'
+import { styled, Popper, TextField } from '@material-ui/core'
 import { Autocomplete } from '@material-ui/lab'
+
+import { platforms } from 'app/data/constants'
 
 const AutoCompletePopper = styled(Popper)({
 	'& .MuiAutocomplete-groupLabel': {
-		textTransform: 'uppercase',
-	},
+		textTransform: 'uppercase'
+	}
 })
 
-let ResourceSelect = ({ resourceOptions, onChange, renderInput }) => {
-	let getOptions = useCallback(() => {
-		return Object.keys(resourceOptions).reduce((allOptions, type) => {
-			let items = resourceOptions[type]
-			let options = items.map((item) => ({ platform: item, type }))
+const ResourceSelect = ({ resourceType, inputLabel, onChange }) => {
+	let [options, setOptions] = useState([])
 
-			return [...allOptions, ...options]
+	useEffect(() => {
+		// { rifles: []..., smgs: []... }
+		const types = platforms[resourceType]
+		const typeKeys = Object.keys(types)
+
+		// Converts { rifles: ['1', '2'...], smgs: ['3', '4'...]}
+		// into [ { type: 'rifles', platform: '1'}, { type: 'rifles', platform: '2' } ... ]
+		// for labeling
+		const allOptions = typeKeys.reduce((options, type) => {
+			const allPlatforms = types[type]
+			const typeOptions = allPlatforms.map((p) => ({ platform: p, type: type }))
+
+			return options.concat(...typeOptions)
 		}, [])
-	}, [resourceOptions])
+
+		setOptions(allOptions)
+	}, [resourceType])
 
 	return (
 		<Autocomplete
-			PopperComponent={AutoCompletePopper}
-			autoHighlight={true}
-			options={getOptions()}
-			getOptionLabel={(option) => option.platform}
-			groupBy={(option) => option.type}
-			onChange={(evt, value) => onChange(value)}
-			renderInput={renderInput}
+			options={ options }
+			getOptionLabel={ (option) => option.platform }
+			groupBy={ (option) => option.type }
+			onChange={ (evt, value) => onChange(value) }
+			renderInput={ (params) => <TextField { ...params } fullWidth={ true } label={ inputLabel } /> }
+			autoHighlight={ true }
+			PopperComponent={ AutoCompletePopper }
 		/>
 	)
 }
 
 ResourceSelect.propTypes = {
+	inputLabel: PropTypes.string.isRequired,
+	resourceType: PropTypes.string.isRequired,
 	resourceOptions: PropTypes.object.isRequired,
-	onChange: PropTypes.func.isRequired,
-	renderInput: PropTypes.func.isRequired,
+	onChange: PropTypes.func.isRequired
 }
 
 export default ResourceSelect
