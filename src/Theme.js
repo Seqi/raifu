1e904ac0aa31a159db8b6ff051d666e104121886
seqi <tomseqi@live.co.uk>
2021-02-28 13:25:37 +0000

    Use usntable mui theme creator to fix strict mode issue

--- src/Theme.js
+++ src/Theme.js
@@ -1,24 +1,24 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles'
 
 export default createMuiTheme({
 	palette: {
 		type: 'dark',
 		primary: {
-			main: '#CA054D'
+			main: '#CA054D',
 		},
 		background: {
 			default: '#282b35',
-			paper: '#3B3E47'
-		}
+			paper: '#3B3E47',
+		},
 	},
 	typography: {
 		fontFamily: ['Alata', 'sans-serif'].join(','),
 		h2: {
-			fontSize: '4rem'
+			fontSize: '4rem',
 		},
 		h5: {
-			fontSize: '1.4rem'
-		}
+			fontSize: '1.4rem',
+		},
 	},
 	breakpoints: {
 		values: {
@@ -26,26 +26,26 @@ export default createMuiTheme({
 			sm: 600,
 			md: 960,
 			lg: 1280,
-			xl: 1600
-		}
+			xl: 1600,
+		},
 	},
 	overrides: {
 		MuiCardHeader: {
 			root: {
-				textTransform: 'uppercase'
-			}
+				textTransform: 'uppercase',
+			},
 		},
 		MuiSpeedDial: {
 			root: {
 				position: 'fixed',
 				bottom: '2%',
-				right: '3%'
-			}
+				right: '3%',
+			},
 		},
 		MuiChip: {
 			root: {
-				textTransform: 'uppercase'
-			}
-		}
-	}
+				textTransform: 'uppercase',
+			},
+		},
+	},
 })
