3737b020ee3fc8534388cab83cfd3242eac0594c
seqi <tomseqi@live.co.uk>
2021-03-06 18:28:07 +0000

    Use new properties to reduce some client side code

--- src/app/features/events/Event/EventActions.js
+++ src/app/features/events/Event/EventActions.js
@@ -1,18 +1,13 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { SpeedDial, SpeedDialAction } from '@material-ui/lab'
 
 import ConfirmDeleteDialog from 'app/shared/actions/delete/ConfirmDeleteDialog'
 import useIsPageAtBottom from 'app/shared/hooks/useIsPageAtBottom'
-import { UserContext } from 'app/core/auth/contexts'
 
 import EventChecklistDialog from './dialogs/EventChecklistDialog'
 import EditEventDialog from '../EditEventDialog'
-
-let isMyEvent = (user, event) => {
-	return event.organiser_uid === user.uid
-}
 
 let getMyLoadout = (event) => {
 	return event.users.length > 0 && event.users[0].loadout
@@ -22,15 +17,12 @@ function EventActions({ event, updateEvent, deleteEvent }) {
 	let [dialog, setDialog] = useState()
 	let [speedDialOpen, setSpeedDialOpen] = useState(false)
 
-	let user = useContext(UserContext)
-
 	let isAtBottom = useIsPageAtBottom()
 	let isInvite = event.users.length === 0
-	let canModify = isMyEvent(user, event)
-	let canViewChecklist = event.users.length > 0 && event.users[0].loadout != null
+	let canViewChecklist = !!getMyLoadout(event)
 
 	// Hide the entire speed dial if no actions are available
-	let hasAvailableActions = canModify || canViewChecklist
+	let hasAvailableActions = event.owner || canViewChecklist
 
 	return (
 		<React.Fragment>
@@ -43,7 +35,7 @@ function EventActions({ event, updateEvent, deleteEvent }) {
 				open={ speedDialOpen }
 				hidden={ isAtBottom || isInvite || !hasAvailableActions }
 			>
-				{canModify && (
+				{event.owner && (
 					<SpeedDialAction
 						icon={ <i className='fa fa-pen' /> }
 						onClick={ () => setDialog('edit') }
@@ -52,7 +44,7 @@ function EventActions({ event, updateEvent, deleteEvent }) {
 					/>
 				)}
 
-				{canModify && (
+				{event.owner && (
 					<SpeedDialAction
 						icon={ <i className='fa fa-trash' /> }
 						onClick={ () => setDialog('delete') }
@@ -72,7 +64,7 @@ function EventActions({ event, updateEvent, deleteEvent }) {
 			</SpeedDial>
 
 			{/* Dialogs */}
-			{canModify && (
+			{event.owner && (
 				<EditEventDialog
 					event={ event }
 					isOpen={ dialog === 'edit' }
@@ -82,7 +74,7 @@ function EventActions({ event, updateEvent, deleteEvent }) {
 				/>
 			)}
 
-			{canModify && (
+			{event.owner && (
 				<ConfirmDeleteDialog
 					verb='Delete'
 					title={ event.getTitle() }
@@ -110,5 +102,5 @@ export default EventActions
 EventActions.propTypes = {
 	event: PropTypes.object.isRequired,
 	updateEvent: PropTypes.func.isRequired,
-	deleteEvent: PropTypes.func.isRequired
+	deleteEvent: PropTypes.func.isRequired,
 }
