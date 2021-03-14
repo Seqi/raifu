6f58e677004f976a37b6820911c43b8e32bda8c4
seqi <tomseqi@live.co.uk>
2021-03-14 20:39:27 +0000

    Type up shared section

--- src/app/features/share/SharedLoadout.tsx
+++ src/app/features/share/SharedLoadout.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import { RouteChildrenProps } from 'react-router-dom'
 
 import { loadouts } from 'app/data/api'
 import { LoadingOverlay, ErrorOverlay } from 'app/shared/state'
 import ReactiveTitle from 'app/shared/text/ReactiveTitle'
 import { LoadoutView } from 'app/features/loadouts'
 import firebase from '../../../firebase'
+import { Loadout } from 'app/shared/models/loadout'
 
 let analytics = firebase.analytics()
 
-export default class SharedLoadout extends React.Component {
-	constructor(props) {
+type SharedLoadoutProps = RouteChildrenProps<{ loadoutId: string }>
+type SharedLoadoutState = {
+	loadout: Loadout | null
+	loading: boolean
+	error: any
+}
+
+export default class SharedLoadout extends React.Component<
+	SharedLoadoutProps,
+	SharedLoadoutState
+> {
+	private unmounted: boolean = false
+
+	constructor(props: SharedLoadoutProps) {
 		super(props)
 
 		this.state = {
 			loadout: null,
 			error: null,
-			loading: true
+			loading: true,
 		}
 	}
 
@@ -27,9 +41,13 @@ export default class SharedLoadout extends React.Component {
 	componentWillUnmount = () => (this.unmounted = true)
 
 	loadLoadout = () => {
+		if (!this.props.match) {
+			throw new Error('No props match')
+		}
+
 		this.setState({ loadout: null, error: null, loading: true }, () => {
 			loadouts
-				.getById(this.props.match.params.loadoutId)
+				.getById(this.props.match!.params.loadoutId)
 				.then((loadout) => {
 					!this.unmounted && this.setState({ loadout: loadout, loading: false })
 				})
@@ -50,14 +68,19 @@ export default class SharedLoadout extends React.Component {
 				return <ErrorOverlay message='Loadout not found.' icon='fa fa-crosshairs' />
 			}
 
-			return <ErrorOverlay message='Could not load loadout.' onRetry={ () => this.loadLoadout() } />
+			return (
+				<ErrorOverlay
+					message='Could not load loadout.'
+					onRetry={ () => this.loadLoadout() }
+				/>
+			)
 		}
 
 		return (
 			<React.Fragment>
-				<ReactiveTitle>{loadout.name}</ReactiveTitle>
+				<ReactiveTitle>{loadout!.name}</ReactiveTitle>
 
-				<LoadoutView loadout={ loadout } editable={ false } />
+				<LoadoutView loadout={ loadout! } editable={ false } />
 			</React.Fragment>
 		)
 	}
