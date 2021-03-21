33a3daaa3a6f603e222abfdd97b99880d6583d00
seqi <tomseqi@live.co.uk>
2021-03-21 22:35:49 +0000

    fix timestamps not being set

--- functions/src/data/database/hooks/order-loadout-items.js
+++ functions/src/data/database/hooks/order-loadout-items.js
@@ -2,22 +2,37 @@ function dateSortByKey(key) {
 	return (item1, item2) => new Date(item1[key].createdAt) - new Date(item2[key].createdAt)
 }
 
+function setTimestamps(item, field) {
+	item.createdAt = item[field].createdAt
+	item.updatedAt = item[field].updatedAt
+}
+
 module.exports = function orderLoadoutItems(loadout) {
 	if (!loadout) {
 		return
 	}
 
 	loadout.weapons.sort(dateSortByKey('loadout_weapon'))
+	loadout.weapons.forEach((weapon) => {
+		setTimestamps(weapon, 'loadout_weapon')
+	})
 
 	if (loadout.gear) {
 		loadout.gear.sort(dateSortByKey('loadout_gear'))
+		loadout.gear.forEach((gear) => {
+			setTimestamps(gear, 'loadout_gear')
+		})
 
 		loadout.gear.forEach((gear) => delete gear.loadout_gear)
 	}
 
 	if (loadout.clothing) {
 		loadout.clothing.sort(dateSortByKey('loadout_clothing'))
-		loadout.clothing.forEach((clothing) => delete clothing.loadout_clothing)
+		loadout.clothing.forEach((clothing) => {
+			setTimestamps(clothing, 'loadout_clothing')
+		})
+
+		loadout.gear.forEach((gear) => delete gear.loadout_clothing)
 	}
 
 	loadout.weapons.forEach((weapon) => {
@@ -25,6 +40,10 @@ module.exports = function orderLoadoutItems(loadout) {
 
 		if (weapon.attachments) {
 			weapon.attachments.sort(dateSortByKey('loadout_weapon_attachment'))
+			weapon.attachents.forEach((attachment) => {
+				setTimestamps(attachment, 'loadout_weapon_attachments')
+			})
+
 			weapon.attachments.forEach(
 				(attachment) => delete attachment.loadout_weapon_attachment
 			)
