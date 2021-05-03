68ac249220c5ac4adc3c4cd5ab6ab561a818ff91
seqi <tomseqi@live.co.uk>
2021-05-03 21:16:04 +0100

    Pass in variant

--- src/app/shared/text/SidewaysTitle.tsx
+++ src/app/shared/text/SidewaysTitle.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react'
 import PropTypes from 'prop-types'
 
+import { Variant } from '@material-ui/core/styles/createTypography'
+
 import { Box, BoxProps, styled, Typography, Slide } from '@material-ui/core'
 
 type SidewaysTitleProps = BoxProps & {
 	title: string
+	variant?: Variant
 }
 
 const ResourceListTitle = styled(Box)(({ theme }) => ({
@@ -19,11 +22,15 @@ const ResourceListTitleText = styled(Typography)({
 	lineHeight: 1.5,
 })
 
-export const SidewaysTitle: FC<SidewaysTitleProps> = ({ title, ...props }) => (
+export const SidewaysTitle: FC<SidewaysTitleProps> = ({
+	title,
+	variant = 'h3',
+	...props
+}) => (
 	<ResourceListTitle { ...props }>
 		<Slide in={ true } direction='right'>
 			<Box width='50px'>
-				<ResourceListTitleText variant='h3'>{title}</ResourceListTitleText>
+				<ResourceListTitleText variant={ variant }>{title}</ResourceListTitleText>
 			</Box>
 		</Slide>
 	</ResourceListTitle>
@@ -31,4 +38,9 @@ export const SidewaysTitle: FC<SidewaysTitleProps> = ({ title, ...props }) => (
 
 SidewaysTitle.propTypes = {
 	title: PropTypes.string.isRequired,
+	variant: PropTypes.any,
+}
+
+SidewaysTitle.defaultProps = {
+	variant: 'h3',
 }
