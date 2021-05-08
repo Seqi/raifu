eb4adde1e391532150d7ad5d74958fd4d28b9471
seqi <tomseqi@live.co.uk>
2021-05-08 03:01:46 +0100

    make title sticky

--- src/app/features/loadouts/components/Loadout/LoadoutView.tsx
+++ src/app/features/loadouts/components/Loadout/LoadoutView.tsx
@@ -40,7 +40,12 @@ const LoadoutView: FC<LoadoutViewProps> = ({ loadout, editable = false }) => {
 		<LoadoutContextProvider loadout={ loadout } editable={ editable }>
 			<AvailableArmoryContextProvider>
 				<Box display='flex'>
-					<SidewaysTitle title={ loadout!.getTitle() } marginRight={ 2 } lowercase={ true } />
+					<SidewaysTitle
+						title={ loadout!.getTitle() }
+						marginRight={ 2 }
+						lowercase={ true }
+						textProps={ { style: { position: 'sticky', top: '10%' } } }
+					/>
 
 					<Box flex='1'>
 						<LoadoutContainer>
