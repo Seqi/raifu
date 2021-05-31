07f9bd6acd19f6d7ad8b8b6b0f5343dfd4f1d558
seqi <tomseqi@live.co.uk>
2021-05-31 14:04:21 +0100

    scale loadout wepaon title size

--- src/app/features/loadouts/components/Loadout/WeaponList/Weapon/LoadoutWeaponView.tsx
+++ src/app/features/loadouts/components/Loadout/WeaponList/Weapon/LoadoutWeaponView.tsx
@@ -1,17 +1,27 @@
 import { FC } from 'react'
 import PropTypes from 'prop-types'
 
-import { Box, Slide, styled } from '@material-ui/core'
+import { Box, Slide, styled, Typography } from '@material-ui/core'
 
 import LoadoutWeaponItem from './LoadoutWeaponItem'
 import LoadoutWeaponAttachmentList from './LoadoutWeaponAttachmentList'
 import { LoadoutWeapon, LoadoutWeaponPropType } from '../../../../models'
-import ReactiveTitle from 'app/shared/text/ReactiveTitle'
 
 const LoadoutWeaponContainer = styled(Box)(({ theme }) => ({
 	flexDirection: 'row',
 	[theme.breakpoints.down('xs')]: {
 		flexDirection: 'column',
+	},
+}))
+
+const LoadoutWeaponTitle = styled(Typography)(({ theme }) => ({
+	fontSize: '2rem',
+
+	[theme.breakpoints.down('md')]: {
+		fontSize: '1.8rem',
+	},
+	[theme.breakpoints.down('sm')]: {
+		fontSize: '1.5rem',
 	},
 }))
 
@@ -21,25 +31,22 @@ type LoadoutWeaponProps = {
 
 const LoadoutWeaponView: FC<LoadoutWeaponProps> = ({ weapon }) => {
 	return (
-		<Box>
-			<ReactiveTitle variant='h4' mobileVariant='h5' align='center'>
-				{weapon.getTitle()}
-			</ReactiveTitle>
+		<LoadoutWeaponContainer display='flex'>
+			<Slide in={ true } direction='right'>
+				<Box display='flex' flexDirection='column' flex={ 3 } position='relative'>
+					<LoadoutWeaponTitle variant='h4' align='center'>
+						{weapon.getTitle()}
+					</LoadoutWeaponTitle>
+					<LoadoutWeaponItem weapon={ weapon } />
+				</Box>
+			</Slide>
 
-			<LoadoutWeaponContainer display='flex'>
-				<Slide in={ true } direction='right'>
-					<Box display='flex' flexDirection='column' flex={ 3 } position='relative'>
-						<LoadoutWeaponItem weapon={ weapon } />
-					</Box>
-				</Slide>
-
-				<Slide in={ true } timeout={ 400 } direction='right'>
-					<Box flex={ 5 }>
-						<LoadoutWeaponAttachmentList weapon={ weapon } />
-					</Box>
-				</Slide>
-			</LoadoutWeaponContainer>
-		</Box>
+			<Slide in={ true } timeout={ 400 } direction='right'>
+				<Box flex={ 5 }>
+					<LoadoutWeaponAttachmentList weapon={ weapon } />
+				</Box>
+			</Slide>
+		</LoadoutWeaponContainer>
 	)
 }
 
