f75b2f5f20e93a7d3e28c81519bab3035dd712f5
seqi <tomseqi@live.co.uk>
2021-11-14 20:17:33 +0000

    Set up firebase on nest

--- functions-nest/src/main.ts
+++ functions-nest/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import express, { Express } from 'express'
+import * as functions from 'firebase-functions'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3006)
+const expressServer = express()
+
+const createFunction = async (expressInstance: Express): Promise<void> => {
+	const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance))
+
+	await app.init()
 }
-bootstrap()
+
+export const api = functions.https.onRequest(async (request, response) => {
+	await createFunction(expressServer)
+	expressServer(request, response)
+})
