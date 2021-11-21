35af33961f9b08680175657a9b00afd094107a59
seqi <tomseqi@live.co.uk>
2021-11-21 21:46:47 +0000

    Add basic logger

--- functions-nest/src/app.module.ts
+++ functions-nest/src/app.module.ts
@@ -1,13 +1,42 @@
+import { EntityManager } from '@mikro-orm/core'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
-import { Module } from '@nestjs/common'
+import {
+	Injectable,
+	Logger,
+	MiddlewareConsumer,
+	Module,
+	NestMiddleware,
+} from '@nestjs/common'
+import { NextFunction } from 'express'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 import { FirebaseModule } from './firebase'
+import { FirebaseUserService } from './firebase/services/firebase-user.service'
+import { WeaponModule } from './weapon/weapon.module'
 
 @Module({
-	imports: [FirebaseModule.forRoot(), MikroOrmModule.forRoot()],
+	imports: [FirebaseModule.forRoot(), MikroOrmModule.forRoot(), WeaponModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, Logger],
 })
-export class AppModule {}
+export class AppModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(UserFilterMiddleware).forRoutes('*')
+	}
+}
+
+// Not sure how to do this properly, but this will attach the request-scoped object
+// so that a previous filter will never remain attached.
+// The EM should be scoped to the request, but idk why it isnt? The instance is shared
+// across requests.. This shouldnt happen so idk why it is
+// https://mikro-orm.io/docs/identity-map/#forking-entity-manager
+@Injectable()
+export class UserFilterMiddleware implements NestMiddleware {
+	constructor(private user: FirebaseUserService, private em: EntityManager) {}
+
+	use(req: Request, res: Response, next: NextFunction) {
+		this.em.setFilterParams('test', { uid: this.user })
+		next()
+	}
+}
--- functions-nest/src/main.ts
+++ functions-nest/src/main.ts
@@ -1,20 +1,38 @@
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, Logger, LoggerService } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { ExpressAdapter } from '@nestjs/platform-express'
 import express, { Express } from 'express'
 import * as functions from 'firebase-functions'
 import { AppModule } from './app.module'
-
+import { WinstonModule, utilities } from 'nest-winston'
+import { transports, format } from 'winston'
 const expressServer = express()
 
 const createNestServer = async (expressInstance: Express): Promise<INestApplication> => {
-	const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance))
+	const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance), {
+		logger: WinstonModule.createLogger({
+			level: process.env.LOG_LEVEL || 'debug',
+			transports: [
+				new transports.Console({
+					format: format.combine(
+						format.timestamp(),
+						format.errors({ stack: true }),
+						format.ms(),
+						utilities.format.nestLike('RaifuApi', { prettyPrint: true }),
+					),
+				}),
+			],
+		}),
+	})
 
 	return await app.init()
 }
 
 createNestServer(expressServer)
-	.then(() => console.log('Nest server started.'))
+	.then((app) => {
+		const logger = app.get<LoggerService>(Logger)
+		logger.log('Nest server started.')
+	})
 	.catch((e) => console.error('Nest server failed to start.', e))
 
 export const api = functions.https.onRequest(expressServer)
