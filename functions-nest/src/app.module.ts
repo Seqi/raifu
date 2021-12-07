b7132dac4c2f70932aca70481542a0f3c2901eda
seqi <tomseqi@live.co.uk>
2021-12-07 02:03:15 +0000

    Remove unused middleware

--- functions-nest/src/app.module.ts
+++ functions-nest/src/app.module.ts
@@ -1,9 +1,7 @@
-import { EntityManager } from '@mikro-orm/core'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
-import { Injectable, Logger, MiddlewareConsumer, Module, NestMiddleware } from '@nestjs/common'
-import { NextFunction } from 'express'
+import { Logger, Module } from '@nestjs/common'
 
-import { FirebaseModule, FirebaseUserService } from './firebase'
+import { FirebaseModule } from './firebase'
 import { AppController } from './app.controller'
 import { LoadoutModule } from './resources/loadout'
 import { ResourceModule } from './resources/resource'
@@ -24,22 +22,4 @@ import { Attachment, Clothing, Gear, Weapon } from './entities'
 	controllers: [AppController],
 	providers: [Logger],
 })
-export class AppModule {
-	configure(consumer: MiddlewareConsumer) {
-		consumer.apply(UserFilterMiddleware).forRoutes('*')
-	}
-}
-
-// Not sure how to do this properly.. Could potentially be a case where a user persists
-// The EM should be scoped to the request, but idk why it isnt? The instance is shared
-// across requests.. This shouldnt happen so idk why it is
-// https://mikro-orm.io/docs/identity-map/#forking-entity-manager
-@Injectable()
-export class UserFilterMiddleware implements NestMiddleware {
-	constructor(private user: FirebaseUserService, private em: EntityManager) {}
-
-	use(req: Request, res: Response, next: NextFunction) {
-		this.em.setFilterParams('forUser', { uid: this.user.uid })
-		next()
-	}
-}
+export class AppModule {}
