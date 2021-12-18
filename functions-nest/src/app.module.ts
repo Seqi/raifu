3d1b3f2152776ae4d791a4db4991ade179c54907
seqi <tomseqi@live.co.uk>
2021-12-18 15:19:12 +0000

    rename resource folder to feature

--- functions-nest/src/app.module.ts
+++ functions-nest/src/app.module.ts
@@ -3,10 +3,10 @@ import { Logger, Module } from '@nestjs/common'
 
 import { FirebaseModule } from './firebase'
 import { AppController } from './app.controller'
-import { LoadoutModule } from './resources/loadout'
-import { ResourceModule } from './resources/resource'
+import { LoadoutModule } from './features/loadout'
+import { ResourceModule } from './features/resource'
 import { Attachment, Clothing, Gear, Weapon } from './entities'
-import { ArmoryModule } from './resources/armory/armory.module'
+import { ArmoryModule } from './features/armory/armory.module'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
