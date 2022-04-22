837bf459e904ec17252a0b099272da1a6c44479f
seqi <tomseqi@live.co.uk>
2022-04-22 22:34:05 +0100

    add msw

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,3 +1,9 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom'
+
+import { server } from 'test/server'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
