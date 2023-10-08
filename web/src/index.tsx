986c2cf10d78d60b54ae225db9ab7e94beb21989
seqi <tomseqi@live.co.uk>
2023-10-08 20:21:07 +0100

    install tanstack query

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,19 +3,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { MuiThemeProvider, CssBaseline, Typography } from '@material-ui/core'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import 'core-js/fn/array/flat-map'
 import 'whatwg-fetch'
 
 import Theme from './theme'
 import AppRouter from 'app/core/Router'
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
 	<React.StrictMode>
 		<MuiThemeProvider theme={Theme}>
-			<CssBaseline />
-			<Typography component='div'>
-				<AppRouter />
-			</Typography>
+			<QueryClientProvider client={queryClient}>
+				<CssBaseline />
+				<Typography component='div'>
+					<AppRouter />
+				</Typography>
+			</QueryClientProvider>
 		</MuiThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
