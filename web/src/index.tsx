d86a03b08113bc239986f0ec3d180c4973de84a3
seqi <tomseqi@live.co.uk>
2023-10-08 20:50:33 +0100

    add defaults for query fn

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -9,9 +9,28 @@ import 'core-js/fn/array/flat-map'
 import 'whatwg-fetch'
 
 import Theme from './theme'
+import app from './firebase'
 import AppRouter from 'app/core/Router'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			queryFn: ({ queryKey }) => {
+				const region = 'us-central1'
+				const projectId = (app.options as any).projectId
+
+				const baseCloudUrl = `https://${region}-${projectId}.cloudfunctions.net/api`
+				const baseLocalUrl = `http://localhost:5001/${projectId}/${region}/api`
+
+				const baseUrl =
+					import.meta.env.MODE === 'development' ? baseLocalUrl : baseCloudUrl
+
+				console.log('query key', queryKey)
+				return fetch(`${baseUrl}/${queryKey[0]}`)
+			},
+		},
+	},
+})
 
 ReactDOM.render(
 	<React.StrictMode>
